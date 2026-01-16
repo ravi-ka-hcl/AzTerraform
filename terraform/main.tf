30d75ef991c8ae8ae2db1f96c6dc0321ce4efdf8
ravi-ka-hcl <ravi-ka@hcltech.com>
2026-01-16 12:38:18 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,10 +3,9 @@ terraform {
     resource_group_name  = "Terrafrom-ResGrp"
     storage_account_name = "terraformmysvkstroage"
     container_name       = "terraformstroagecontainter"
-    ASSIGNEE="40f16ccb-0ebe-45ac-a8af-d257fc178f83"
+    ASSIGNEE="e9aacc59-86d2-49fe-94fa-e7c77be530fd"
     key                  = "dev.terraform.tfstate"
-    use_azuread_auth = true
-  }
+    }
 }
 module "RG" {
   source   = "./modules/RG" #A
